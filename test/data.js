0930f05f85283329c4728bedfa7e95e0fab203f2
Micky Hulse <mickyhulse@gmail.com>
2019-02-24 12:12:44 -0800

    SAVEPOINT

--- test/data.js
+++ test/data.js
@@ -6,8 +6,8 @@ module.exports = [
       id: "CAoSLEFGMVFpcE94VEpTc0ExX2hwNExZQkRsdG9MZEFwOTNjdDNYZmwyQm1yNUU3",
       key: "AF1QipOxTJSsA1_hp4LYBDltoLdAp93ct3Xfl2Bmr5E7", // image_key
       zoom: 4,
-      y: 6,
-      x: 13
+      x: 13,
+      y: 6
     },
     output: [
       'https://lh3.ggpht.com/p/AF1QipOxTJSsA1_hp4LYBDltoLdAp93ct3Xfl2Bmr5E7=x13-y6-z4'
@@ -18,8 +18,8 @@ module.exports = [
       id: "CAoSLEFGMVFpcFBUUGE3aHhlb1NWMEUxRFNwWFA5bW56VzZtYVc5VUV2NDJ2Yjc1",
       key: "AF1QipPTPa7hxeoSV0E1DSpXP9mnzW6maW9UEv42vb75",
       zoom: 3,
-      y: 2,
-      x: 4
+      x: 4,
+      y: 2
     },
     output: [
       'https://lh3.ggpht.com/p/AF1QipPTPa7hxeoSV0E1DSpXP9mnzW6maW9UEv42vb75=x4-y2-z3'
@@ -30,8 +30,8 @@ module.exports = [
       id: "CAoSLEFGMVFpcE9Pc0Via19ieFc5T3RnVlRKRzdnZS1WWlRPaFRVakROWkxTUVhm",
       key: "AF1QipOOsEbk_bxW9OtgVTJG7ge-VZTOhTUjDNZLSQXf",
       zoom: 3,
-      y: 3,
-      x: 7
+      x: 7,
+      y: 3
     },
     output: [
       'https://lh3.ggpht.com/p/AF1QipOOsEbk_bxW9OtgVTJG7ge-VZTOhTUjDNZLSQXf=x7-y3-z3'
@@ -42,8 +42,8 @@ module.exports = [
       id: "jtP01lmH5P7CDsZ_Ag5DAA",
       key: "jtP01lmH5P7CDsZ_Ag5DAA",
       zoom: 5,
-      y: 12,
-      x: 25
+      x: 25,
+      y: 12
     },
     output: [
       'https://geo3.ggpht.com/cbk?panoid=jtP01lmH5P7CDsZ_Ag5DAA&output=tile&x=25&y=12&zoom=5&nbt&fover=2&authuser=0&cb_client=maps_sv.tactile'
@@ -54,8 +54,8 @@ module.exports = [
       id: "CAoSLEFGMVFpcFA4VWZQQmJrSnNSY2pPNFlZNEt6VG1IZkw3cHowUHBERlVvNWdp",
       key: "AF1QipP8UfPBbkJsRcjO4YY4KzTmHfL7pz0PpDFUo5gi",
       zoom: 5,
-      y: 8,
-      x: 16
+      x: 16,
+      y: 8
     },
     output: [
       'https://lh3.ggpht.com/p/AF1QipP8UfPBbkJsRcjO4YY4KzTmHfL7pz0PpDFUo5gi=x16-y8-z5'
@@ -66,8 +66,8 @@ module.exports = [
       id: "CAoSLEFGMVFpcE5OUmlHY1Vvczc1V3pvM05WZW1QUWw0WEZCOHVRbGlHYklQUDQ1",
       key: "AF1QipNNRiGcUos75Wzo3NVemPQl4XFB8uQliGbIPP45",
       zoom: 4,
-      y: 7,
-      x: 14
+      x: 14,
+      y: 7
     },
     output: [
       'https://lh3.ggpht.com/p/AF1QipNNRiGcUos75Wzo3NVemPQl4XFB8uQliGbIPP45=x14-y7-z4'
@@ -78,8 +78,8 @@ module.exports = [
       id: "VSez-JAIqV5Uj7Xe3GiJ1w",
       key: "VSez-JAIqV5Uj7Xe3GiJ1w",
       zoom: 5,
-      y: 12,
-      x: 25
+      x: 25,
+      y: 12
     },
     output: [
       'https://geo3.ggpht.com/cbk?panoid=VSez-JAIqV5Uj7Xe3GiJ1w&output=tile&x=25&y=12&zoom=5&nbt&fover=2&authuser=0&cb_client=maps_sv.tactile'
@@ -90,8 +90,8 @@ module.exports = [
       id: "AT8UXtb4kxsiTsILjCXzNg",
       key: "AT8UXtb4kxsiTsILjCXzNg",
       zoom: 5,
-      y: 12,
-      x: 25
+      x: 25,
+      y: 12
     },
     output: [
       'https://geo3.ggpht.com/cbk?panoid=AT8UXtb4kxsiTsILjCXzNg&output=tile&x=25&y=12&zoom=5&nbt&fover=2&authuser=0&cb_client=maps_sv.tactile'
